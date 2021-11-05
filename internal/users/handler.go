bf58a04535762afa44a99c42177e2485b6c2f5b4
Suresh N <suresh.04n@gmail.com>
2021-11-05 20:50:42 +0530

    update: sqlc generated files

--- internal/users/handler.go
+++ internal/users/handler.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"github.com/aveloper/blog/internal/utils"
 	"net/http"
 	"time"
 
@@ -38,7 +39,6 @@ func (h *Handler) addUser() http.HandlerFunc {
 		Name          string    `json:"name"`
 		Email         string    `json:"email"`
 		Role          string    `json:"role"`
-		EmailVerified bool      `json:"email_verified"`
 		CreatedAt     time.Time `json:"created_at"`
 		UpdatedAt     time.Time `json:"updated_at"`
 	}
@@ -51,11 +51,25 @@ func (h *Handler) addUser() http.HandlerFunc {
 			return
 		}
 
+		hash, valid ,err := utils.ValidateAndHashPassword(req.Password)
+		if !valid {
+			if err == nil {
+				h.log.Error("Password is not meeting the requirement "+ req.Password)
+				h.jsonWriter.InvalidPasswordErr(w, r)
+			} else {
+				//TODO: need to change the DefaultError method
+				h.log.Error("Failed to hash the password ", zap.Error(err))
+				h.jsonWriter.DefaultError(w, r)
+			}
+
+			return
+		}
+
 		// FIXME: Hash password before saving
 		user, err := h.repository.AddUser(r.Context(), query.AddUserParams{
 			Name:     req.Name,
 			Email:    req.Email,
-			Password: req.Password,
+			Password: hash,
 			Role:     query.UserRole(req.Role),
 		})
 		if err != nil {
@@ -70,12 +84,297 @@ func (h *Handler) addUser() http.HandlerFunc {
 			Name:          user.Name,
 			Email:         user.Email,
 			Role:          string(user.Role),
-			EmailVerified: user.EmailVerified,
 			CreatedAt:     user.CreatedAt,
 			UpdatedAt:     user.UpdatedAt,
 		}
 
-		// TODO: Send verification Email
+
+		h.jsonWriter.Ok(w, r, resp)
+	}
+}
+
+func (h *Handler) updateUser() http.HandlerFunc{
+	type Request struct {
+		ID       int32     	`json:"id" validate:"required"`
+		Name     string 		`json:"name" validate:"required"`
+		Email    string 		`json:"email" validate:"required,email"`
+		Role     string 		`json:"role" validate:"required,oneof=admin editor author contributor"`
+	}
+
+	type Response struct {
+		ID            int32     `json:"id"`
+		Name          string    `json:"name"`
+		Email         string    `json:"email"`
+		Role          string    `json:"role"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &Request{}
+
+		ok := h.reader.ReadJSONAndValidate(w, r, req)
+		if !ok {
+			return
+		}
+
+		user, err := h.repository.UpdateUser(r.Context(), query.UpdateUserParams{
+			ID: 			req.ID,
+			Name:     req.Name,
+			Email:    req.Email,
+			Role:     query.UserRole(req.Role),
+		})
+		if err != nil {
+			// TODO: Handle user constraint errors separately
+			h.log.Error("Failed adding user to DB", zap.Error(err))
+			h.jsonWriter.DefaultError(w, r)
+			return
+		}
+
+		resp := &Response{
+			ID:            user.ID,
+			Name:          user.Name,
+			Email:         user.Email,
+			Role:          string(user.Role),
+			CreatedAt:     user.CreatedAt,
+			UpdatedAt:     user.UpdatedAt,
+		}
+
+
+		h.jsonWriter.Ok(w, r, resp)
+	}
+}
+
+func (h *Handler) updateUserName() http.HandlerFunc{
+	type Request struct {
+		ID       int32     	`json:"id" validate:"required"`
+		Name     string `json:"name" validate:"required"`
+	}
+
+	type Response struct {
+		ID            int32     `json:"id"`
+		Name          string    `json:"name"`
+		Email         string    `json:"email"`
+		Role          string    `json:"role"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &Request{}
+
+		ok := h.reader.ReadJSONAndValidate(w, r, req)
+		if !ok {
+			return
+		}
+
+		user, err := h.repository.UpdateUserName(r.Context(), query.UpdateUserNameParams{
+			ID: 			req.ID,
+			Name:     req.Name,
+		})
+		if err != nil {
+			h.log.Error("Failed updating user name to DB", zap.Error(err))
+			h.jsonWriter.DefaultError(w, r)
+			return
+		}
+
+		resp := &Response{
+			ID:            user.ID,
+			Name:          user.Name,
+			Email:         user.Email,
+			Role:          string(user.Role),
+			CreatedAt:     user.CreatedAt,
+			UpdatedAt:     user.UpdatedAt,
+		}
+
+
+		h.jsonWriter.Ok(w, r, resp)
+	}
+}
+
+func (h *Handler) updateUserEmail() http.HandlerFunc{
+	type Request struct {
+		ID       int32     	`json:"id" validate:"required"`
+		Email    string `json:"email" validate:"required,email"`
+	}
+
+	type Response struct {
+		ID            int32     `json:"id"`
+		Name          string    `json:"name"`
+		Email         string    `json:"email"`
+		Role          string    `json:"role"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &Request{}
+
+		ok := h.reader.ReadJSONAndValidate(w, r, req)
+		if !ok {
+			return
+		}
+
+		user, err := h.repository.UpdateUserEmail(r.Context(), query.UpdateUserEmailParams{
+			ID:				req.ID,
+			Email:    req.Email,
+		})
+		if err != nil {
+			h.log.Error("Failed updating user email to DB", zap.Error(err))
+			h.jsonWriter.DefaultError(w, r)
+			return
+		}
+
+		resp := &Response{
+			ID:            user.ID,
+			Name:          user.Name,
+			Email:         user.Email,
+			Role:          string(user.Role),
+			CreatedAt:     user.CreatedAt,
+			UpdatedAt:     user.UpdatedAt,
+		}
+
+
+		h.jsonWriter.Ok(w, r, resp)
+	}
+}
+
+func (h *Handler) updateUserPassword() http.HandlerFunc{
+	type Request struct {
+		ID       int32     	`json:"id" validate:"required"`
+		Password string `json:"password" validate:"required,gte=8"`
+	}
+
+	type Response struct {
+		ID            int32     `json:"id"`
+		Name          string    `json:"name"`
+		Email         string    `json:"email"`
+		Role          string    `json:"role"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &Request{}
+
+		ok := h.reader.ReadJSONAndValidate(w, r, req)
+		if !ok {
+			return
+		}
+
+		hash, valid ,err := utils.ValidateAndHashPassword(req.Password)
+		if !valid {
+			if err == nil {
+				h.log.Error("Password is not meeting the requirement "+ req.Password)
+				h.jsonWriter.InvalidPasswordErr(w, r)
+			} else {
+				//TODO: need to change the DefaultError method
+				h.log.Error("Failed to hash the password ", zap.Error(err))
+				h.jsonWriter.DefaultError(w, r)
+			}
+
+			return
+		}
+
+		user, err := h.repository.UpdateUserPassword(r.Context(), query.UpdateUserPasswordParams{
+			ID: 			req.ID,
+			Password: hash,
+		})
+		if err != nil {
+			// TODO: Handle user constraint errors separately
+			h.log.Error("Failed updating user password to DB", zap.Error(err))
+			h.jsonWriter.DefaultError(w, r)
+			return
+		}
+
+		resp := &Response{
+			ID:            user.ID,
+			Name:          user.Name,
+			Email:         user.Email,
+			Role:          string(user.Role),
+			CreatedAt:     user.CreatedAt,
+			UpdatedAt:     user.UpdatedAt,
+		}
+
+
+		h.jsonWriter.Ok(w, r, resp)
+	}
+}
+
+func (h *Handler) updateUserRole() http.HandlerFunc{
+	type Request struct {
+		ID       int32     	`json:"id" validate:"required"`
+		Role     string `json:"role" validate:"required,oneof=admin editor author contributor"`
+	}
+
+	type Response struct {
+		ID            int32     `json:"id"`
+		Name          string    `json:"name"`
+		Email         string    `json:"email"`
+		Role          string    `json:"role"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &Request{}
+
+		ok := h.reader.ReadJSONAndValidate(w, r, req)
+		if !ok {
+			return
+		}
+
+		user, err := h.repository.UpdateUserRole(r.Context(), query.UpdateUserRoleParams{
+			ID:				req.ID,
+			Role:     query.UserRole(req.Role),
+		})
+		if err != nil {
+			h.log.Error("Failed updating user role to DB", zap.Error(err))
+			h.jsonWriter.DefaultError(w, r)
+			return
+		}
+
+		resp := &Response{
+			ID:            user.ID,
+			Name:          user.Name,
+			Email:         user.Email,
+			Role:          string(user.Role),
+			CreatedAt:     user.CreatedAt,
+			UpdatedAt:     user.UpdatedAt,
+		}
+
+
+		h.jsonWriter.Ok(w, r, resp)
+	}
+}
+
+func (h *Handler) DeleteUser() http.HandlerFunc {
+	type Request struct {
+		ID            int32     `json:"id" validate:"required"`
+	}
+
+	type Response struct {
+		Message				string		`json:"message"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &Request{}
+
+		ok := h.reader.ReadJSONAndValidate(w, r, req)
+		if !ok {
+			return
+		}
+
+		err := h.repository.DeleteUser(r.Context(), req.ID)
+		if err != nil {
+			h.log.Error("Failed to delete user from DB", zap.Error(err))
+			h.jsonWriter.DefaultError(w, r)
+			return
+		}
+
+		resp := &Response{
+			Message: "User successfully deleted",
+		}
 
 		h.jsonWriter.Ok(w, r, resp)
 	}
--- internal/users/route.go
+++ internal/users/route.go
@@ -11,4 +11,11 @@ func UserRoutes(r *mux.Router, uh *Handler) {
 	r = r.PathPrefix("/users").Subrouter()
 
 	r.HandleFunc("/", uh.addUser()).Methods(http.MethodPost)
+	r.HandleFunc("/edit", uh.updateUser()).Methods(http.MethodPost)
+	r.HandleFunc("/edit/name", uh.updateUserName()).Methods(http.MethodPost)
+	r.HandleFunc("/edit/role", uh.updateUserRole()).Methods(http.MethodPost)
+	r.HandleFunc("/edit/email", uh.updateUserEmail()).Methods(http.MethodPost)
+	r.HandleFunc("/edit/password", uh.updateUserPassword()).Methods(http.MethodPost)
+	//FIXME: it can also be changed to /delete/{id} for better use
+ 	r.HandleFunc("/delete", uh.DeleteUser()).Methods(http.MethodPost)
 }
